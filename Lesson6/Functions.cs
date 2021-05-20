f954c0b204675f921a0575d3c3feab1c5634eae2
danila <daniil.grishel@gmail.com>
2021-05-20 17:16:00 +0300

    Добавьте файлы проекта.

---
+++ Lesson6/Functions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6
+{
+    class Functions
+    {
+        public double MyFunc(double a, double x)
+        {
+            return a * x * x;
+        }
+        public double Sin(double a, double x)
+        {
+            return a * Math.Sin(x);
+        }
+    }
+}
---
+++ Lesson6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Lesson6
+{
+    class Program : Create
+    {
+        static void Main()
+        {
+            //1. Изменить программу вывода таблицы функции так, чтобы можно было передавать функции типа double (double, double). Продемонстрировать работу на функции с функцией a*x^2 и функцией a*sin(x).
+            //Гришель
+            Menu menu = new Menu();
+            menu.Start();
+        }
+    }
+
+}
